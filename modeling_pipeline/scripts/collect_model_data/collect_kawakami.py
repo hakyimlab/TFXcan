bec1ac2abd766db86bfae938bf2260d51e51fd54
Temi <temi@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2022-12-20 20:45:34 +0000

    Updated repo

--- modeling_pipeline/scripts/collect_model_data/collect_kawakami.py
+++ modeling_pipeline/scripts/collect_model_data/collect_kawakami.py
@@ -1,20 +1,26 @@
 import pandas as pd
 import os, sys, tqdm
 from datetime import date
+import parsl
+import subprocess
+from parsl.app.app import python_app
 
 todays_date = date.today().strftime("%Y-%m-%d")
 
-#agg_type = sys.argv[1] #'aggByMean' # data_names = ['aggByMean', 'aggByCenter', 'aggByUpstream', 'aggByDownstream', 'aggByUpstreamDownstream']
 
 import utility_functions
 
-
 #prediction_path=/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/enformer_predictions/kawakami/predictions_2022-12-20/kawakami,log_dir=/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/predictions_log/kawakami/predictions_log_2022-12-20,id=kawakami,agg_type=aggByMean
 
-enformer_predictions_path = "/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/enformer_predictions/kawakami/predictions_2022-12-20/kawakami" #sys.argv[1] 
-log_path = "/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/predictions_log/kawakami/predictions_log_2022-12-20" #sys.argv[2]
-each_id = "kawakami" #sys.argv[3] # e.g. "kawakami" or "cistrome"
-agg_type = "aggByMean" #sys.argv[4]
+# enformer_predictions_path = "/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/enformer_predictions/kawakami/predictions_2022-12-20/kawakami" #sys.argv[1] 
+# log_path = "/lus/grand/projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer_pipeline/predictions_log/kawakami/predictions_log_2022-12-20" #sys.argv[2]
+# each_id = "kawakami" #sys.argv[3] # e.g. "kawakami" or "cistrome"
+# agg_type = "aggByMean" #sys.argv[4]
+
+enformer_predictions_path = sys.argv[1] 
+log_path = sys.argv[2]
+each_id = sys.argv[3] # e.g. "kawakami" or "cistrome"
+agg_type = sys.argv[4]
 
 print(f'[INFO] Currently on {each_id}')
 
@@ -36,8 +42,32 @@ TF = 'FOXA1'
 
 logpath = f'{log_path}/{each_id}_{TF}_predictions_log.csv'
 log_data = pd.read_csv(logpath)
-log_data = log_data.drop_duplicates(subset=['motif'])
+log_data = log_data.drop_duplicates(subset=['motif'])#.iloc[1:1000, ]
 
-#exec(open(f'{base_path}/modeling_pipeline/scripts/collect_model_data/utility-functions.py').read(), globals(), globals())
-collected = utility_functions.collect_modeling_data_for_kawakami(each_id=each_id, log_data=log_data, predictions_path=enformer_predictions_path, TF=TF, agg_types=[agg_type], base_path=base_path, save_dir=save_dir)
-print(f'[INFO] Status: {collected} for {log_data.shape[0]} predictions for {each_id}.')
+# use parsl if the num of rows of log_data is more than 10000
+use_parsl = True
+print(f'[INFO] Using parsl. Aggregation will be split into multiple files. Collect the batches into one later.')
+
+bpath = os.path.join(base_path, 'modeling_pipeline')
+parsl.load(utility_functions.localParslConfig_threadpool({'working_dir': bpath}))
+
+collection_fxn = utility_functions.return_prediction_function(use_parsl)
+range_batches = utility_functions.generate_batch(range(0, log_data.shape[0]), batch_n=5)
+
+app_futures = []
+for i, range_batch in enumerate(range_batches):
+
+    app_futures.append(collection_fxn(each_id=each_id, log_data=log_data.iloc[range_batch, ], predictions_path=enformer_predictions_path, TF=TF, agg_types=[agg_type], base_path=base_path, save_dir=save_dir, batch_num=i))
+
+app_execs = [r.result() for r in app_futures]
+
+cmd = f"for i in `ls {save_dir}/*_batch_*.csv.gz`; do zcat $i | sed '1d'; done | pigz -c >{save_dir}/{each_id}_{agg_type}_{TF}.csv.gz"    
+
+p = subprocess.Popen(cmd, shell=True)
+
+# remove the files
+
+
+print(f'[INFO] Status: Aggregation complete for {log_data.shape[0]} predictions for {each_id}.')
+
+# for i in `ls ./*.gz`; do zcat \"$i\" | sed '1d'; done > k_aggByMean_T_compiled.csv & gzip -f k_aggByMean_T_compiled.csv
