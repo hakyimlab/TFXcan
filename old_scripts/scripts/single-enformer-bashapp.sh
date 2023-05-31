b2985e12fe3531f294bc916122356a3b2b827733
Temi <temi@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2023-05-31 13:49:56 +0000

    Updated repo

--- old_scripts/scripts/single-enformer-bashapp.sh
+++
@@ -1,3 +0,0 @@
-python3 /projects/covid-ct/imlab/users/temi/projects/TFXcan/enformer-minimal/scripts/single-enformer-run.py \
---sequence_region $1 \
---sequence_sample $2
--- scripts/run_tfpred.sh
+++
@@ -1,4 +0,0 @@
-
-# run enformer
-conda activate /lus/grand/projects/TFXcan/imlab/shared/software/conda_envs/enformer-predict-tools
-python3 /lus/grand/projects/TFXcan/imlab/shared/pipelines/enformer_pipeline/scripts/enformer_predict.py --parameters /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/metadata/cwas_imputed_config.json  
