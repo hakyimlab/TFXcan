a98f2e1841d2e394dd1802f9b5c0786b3dc12b94
Temi <temi@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2023-07-26 12:58:01 +0000

    updated repo

--- scripts/tfpred/enet_evaluate_individuals.R
+++ scripts/tfpred/enet_evaluate_individuals.R
@@ -1,4 +1,16 @@
-arguments <- commandArgs(trailingOnly=TRUE)
+suppressPackageStartupMessages(library("optparse"))
+
+option_list <- list(
+    make_option("--model_rds", help='The command to run.'),
+    make_option("--predict_on", default=NULL, help = "Prefix of files before the extension."),
+    make_option("--individuals_data_dir", default=NULL, help = "Prefix of files before the extension."),
+    make_option("--output_file", default=NULL, help="Full path plus prefix of output file."),
+    make_option("--agg_method", default=NULL, help='Full path to output folder'),
+    make_option("--metadata", default=NULL, help='Full path to output folder')
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
 #print(arguments)
 
 library(glue)
@@ -7,197 +19,78 @@ library(data.table)
 library(parallel)
 library(tidyverse)
 
-<<<<<<< HEAD
-model_dir <- arguments[1] # /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/TFPred_pipeline/models/cistrome_AR_aggByMeanCenter_linear_2023-01-24.rds
-predict_on <- arguments[2] # '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/compare_predictors/metadata/cwas_individuals.txt'
-individuals_data_dir <- arguments[3] #'/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/predictions_folder/cwas_AR_Prostate/predictions_2023-05-16/aggregated_predictions'
-output_dir <- arguments[4] # /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/output
-agg_method <- arguments[5] # aggByMeanCenter
-metainfo <- arguments[6] # e.g. AR_Prostate_1KG'
-id_num <- arguments[7] # e.g. 00, 01, 02 e.t.c
-
-
-# model_dir <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFPred_models/models/cistrome_AR_Prostate_2023-05-16'
-# predict_on <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/compare_predictors/metadata/cwas_individuals.txt'
-# individuals_data_dir <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/predictions_folder/cwas_AR_Prostate/predictions_2023-05-16/aggregated_predictions'
-# output_dir <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/output'
-# agg_method <- 'aggByMeanCenter'
-# metainfo <- 'AR_Prostate_1KG'
-# id_num <- 00
-=======
-model_rds <- arguments[1] # /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/TFPred_pipeline/models/cistrome_AR_aggByMeanCenter_linear_2023-01-24.rds
-predict_on <- arguments[2] # /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/info/cwas_individuals.txt
-model_type <- arguments[3] # linear
-individuals_data_dir <- arguments[4] #'/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/predictions_folder/cwas_AR_Prostate/predictions_2023-05-16/aggregated_predictions'
-output_rds <- arguments[5] # /lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/output
-
-# model_rds='/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/TFPred_pipeline/models/cistrome_AR_aggByMeanCenter_linear_2023-01-24.rds'
-# predict_on='/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/metadata/cwas_individuals.txt'
-# model_type='linear'
-# individuals_data_dir='/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/predictions_folder/cwas_AR_Prostate/predictions_2023-05-16/aggregated_predictions'
-# output_dir='/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/output'
-
-# model_rds <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFPred_models/models/cistrome_AR_Prostate_2023-05-16/aggByMeanCenter_AR_Prostate.logistic.rds' 
-# predict_on <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/metadata/cwas_individuals.txt' 
-# model_type <- 'logistic' 
-# individuals_data_dir <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/predictions_folder/cwas_imputed_AR_Prostate/predictions_2023-05-30/aggregated_predictions' 
-#output_rds <- '/lus/grand/projects/TFXcan/imlab/users/temi/projects/TFXcan/experiments/AR_prostate/output/imputed_cwas_predictions/aggByMeanCenter_AR_Prostate_cwas.logistic.rds'
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
 
 #print(glue('model dir is {model_dir}\nTF is {TF}\nmodel_id is {model_id}\noutput directory is {output_dir}\nrun date is {run_date}\nmodel type is {model_type}\n\n'))
 
 # individuals
-individuals <- data.table::fread(predict_on, header=F)
+individuals <- data.table::fread(opt$predict_on, header=F)
 individuals <- individuals$V1 |> unique()#[1:5]
 print(individuals[1:5])
-<<<<<<< HEAD
-#agg_method <- 'aggByMeanCenter'
 
-# read in the models
-# load all the models coefficients
-models_list <- purrr::map(.x=agg_method, function(each_method){
-    linear_model <- readRDS(glue('{model_dir}/{each_method}_{metainfo}.linear.rds'))
-    logistic_model <- readRDS(glue('{model_dir}/{each_method}_{metainfo}.logistic.rds'))
-    model <- list(linear=linear_model, logistic=logistic_model)
-    return(model)
-}, .progress=T)
+# read in the model
+if(file.exists(opt$model_rds)){
+    #print(glue('INFO - Found model at {opt$model_rds}.'))
+    model <- readRDS(opt$model_rds)
+} else {
+    stop('ERROR - model rds file does not exist.')
+}
 
-names(models_list) <- agg_method
-
-model_types <- c('linear', 'logistic')
-=======
-agg_methods <- 'aggByMeanCenter'
-
-# read in the models
-# load all the models coefficients
-models_list <- purrr::map(.x=agg_methods, function(each_method){
-    model <- readRDS(model_rds)
-    return(model)
-}, .progress=T)
-
-names(models_list) <- agg_methods
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
-
-# read in the individual's training data & merge with the ground truth and get the pscores
-#ground_truth_file <- data.table::fread(individuals_ground_truth_file) |> as.data.frame()
-
-# first gather valid
+# first gather valid : check that the files exist
 valid_names <- c()
 for(name in individuals){
-<<<<<<< HEAD
-    ind_gt <- glue('{individuals_data_dir}/{name}_{agg_method}_{metainfo}.csv')
+    ind_gt <- glue('{opt$individuals_data_dir}/{name}_{opt$agg_method}_{opt$metadata}.csv')
     if(file.exists(ind_gt)){
-        print(name)
-=======
-    ind_gt <- glue('{individuals_data_dir}/{name}_aggByMeanCenter_AR_Prostate.csv')
-    if(file.exists(ind_gt)){
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
         valid_names <- append(valid_names, name)
     }
 }
 print(glue('INFO - Found {length(valid_names)} valid individuals'))
 
-<<<<<<< HEAD
-# here I am using just the linear model
-for(model_type in model_types[1]){
-    #model_type <- 'linear'
-
-    predictions_list <- parallel::mclapply(valid_names, function(each_ind){
-        #each_ind <- 'DFCI_1636'
-        ind_gt <- glue('{individuals_data_dir}/{each_ind}_{agg_method}_{metainfo}.csv')
-        if(file.exists(ind_gt)){
-            mat_dt <- data.table::fread(ind_gt)
-        }
-        #out <- lapply(agg_method, function(each_method){
-=======
-predictions_list <- parallel::mclapply(valid_names, function(each_ind){
-
-    #ind_gt <- ground_truth_file[, c('region', each_ind)]
-   # print(each_ind)
-
-    ind_gt <- glue('{individuals_data_dir}/{each_ind}_aggByMeanCenter_AR_Prostate.csv')
+start_time <- proc.time()
+ncores <- 32
+model_predictions <- parallel::mclapply(valid_names, function(each_ind){
+    ind_gt <- glue('{opt$individuals_data_dir}/{each_ind}_{opt$agg_method}_{opt$metadata}.csv')
     if(file.exists(ind_gt)){
         mat_dt <- data.table::fread(ind_gt)
-    }
-
-    out <- lapply(agg_methods, function(each_method){
-
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
-        # compute prediction scores
-        newx <- as.matrix(mat_dt[, -c(1)])
-        regions <- mat_dt[, 1]
-        if(model_type == 'linear'){
-<<<<<<< HEAD
-            # you only need one : link
-            link_pred <- predict(models_list[[agg_method]][[model_type]], newx, s = "lambda.1se", type = 'link') |> as.vector()
-=======
-
-            # you only need one : link
-            link_pred <- predict(models_list[[each_method]], newx, s = "lambda.1se", type = 'link') |> as.vector()
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
-            df <- cbind(regions, link_pred) |> as.data.frame() #new_dt[, c(1:2)] |> as.data.frame()
-            colnames(df) <- c('regions', 'prediction_link')
-
-        } else if (model_type == 'logistic'){
-            # return two things
-<<<<<<< HEAD
-            link_pred <- predict(models_list[[agg_method]][[model_type]], newx, s = "lambda.1se", type = 'link') |> as.vector()
-            response_pred <- predict(models_list[[agg_method]][[model_type]], newx, s = "lambda.1se", type = 'response') |> as.vector()
-            df <- cbind(regions, link_pred, response_pred) |> as.data.frame()
-            colnames(df) <- c('regions', 'prediction_link', 'prediction_response')
-        }
-
-        # merge by region, and class => they should all be the same but this provides a sanity check
-        return(df)
-
-    }, mc.cores=16)
-
-    #predictions_list <- list(a=1, b=2)
-    names(predictions_list) <- valid_names
-    out_dt <- purrr::reduce(predictions_list, dplyr::left_join, by=c('regions'))
-    colnames(out_dt) <- c('locus', valid_names)
-
-    # save the object to be read later
-    output_file <- glue('{output_dir}/{agg_method}_{metainfo}_1KG.{model_type}.{id_num}.txt')
-    if(!dir.exists(dirname(output_file))){
-        dir.create(dirname(output_file))
+        #print(glue('INFO - Dimension of data is {dim(mat_dt)}'))
     } else {
-        print(glue('INFO - {dirname(output_file)} exists'))
+        #print(glue('INFO - {each_ind} predictors does not exist.'))
+        next
     }
+    #out <- lapply(agg_method, function(each_method){
+    # compute prediction scores
+    newx <- as.matrix(mat_dt[, -c(1)])
+    locus <- mat_dt[, 1]
+    link_pred <- predict(model, newx, s = "lambda.1se", type = 'link') |> as.vector()
+    df <- cbind(locus, link_pred) |> as.data.frame() #new_dt[, c(1:2)] |> as.data.frame()
+    colnames(df) <- c('locus', 'tfpred_score')
+    print(glue('INFO - Dimension of predictions is {dim(df)}'))
 
-    print(glue('INFO - Saving to {output_file}'))
-    data.table::fwrite(out_dt, file=output_file, quote=FALSE, row.names=FALSE)
-}
+    # merge by locus, and class => they should all be the same but this provides a sanity check
+    return(df)
 
-=======
+}, mc.cores=ncores)
 
-            link_pred <- predict(models_list[[each_method]], newx, s = "lambda.1se", type = 'link') |> as.vector()
-            response_pred <- predict(models_list[[each_method]], newx, s = "lambda.1se", type = 'response') |> as.vector()
-            df <- cbind(regions, link_pred, response_pred) |> as.data.frame()
-            colnames(df) <- c('regions', 'prediction_link', 'prediction_response')
-        }
-        return(df)
-    })
+#predictions_list <- list(a=1, b=2)
+#names(model_predictions) <- valid_names
+out_dt <- purrr::reduce(model_predictions, dplyr::left_join, by=c('locus'))
+colnames(out_dt) <- c('locus', valid_names)
+#out_dt <- out_dt %>% tibble::column_to_rownames('locus') %>% as.matrix()
 
-    # merge by region, and class => they should all be the same but this provides a sanity check
-    names(out) <- agg_methods
-    return(out)
-
-}, mc.cores=16)
-
-names(predictions_list) <- valid_names
-
-print(glue('INFO - Length of predictions list is {length(predictions_list)}'))
+end_time <- proc.time()
 
 # save the object to be read later
-
-if(!dir.exists(dirname(output_rds))){
-    dir.create(dirname(output_rds))
+if(!dir.exists(dirname(opt$output_file))){
+    dir.create(dirname(opt$output_file))
 } else {
-    print(glue('INFO - {dirname(output_rds)} already exists'))
+    print(glue('INFO - {dirname(opt$output_file)} exists'))
 }
 
-print(glue('INFO - Saving to {output_rds}'))
-#rds_file <- glue('{output_dir}/cwas_AR_Prostate_linear.rds')
-saveRDS(predictions_list, file=output_rds)
->>>>>>> 8d1e50b0eb3dd8bdfc1db9105ea1984253c9762d
+print(glue('INFO - Saving to {opt$output_file}'))
+data.table::fwrite(out_dt, file=opt$output_file, quote=FALSE, row.names=FALSE, compress = 'gzip')
+
+summary_dt <- rbind(as.matrix(end_time - start_time), ncores = ncores, nindividuals=length(valid_names))
+# save summary
+summary_file <- gsub('.txt.gz', '.summary.txt', opt$output_file)
+sink(summary_file)
+summary_dt
+sink()
