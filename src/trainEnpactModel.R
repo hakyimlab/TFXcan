97cf1b899189d2f1a5ee72a1fe59544957181425
temipete <tadepete@gmail.com>
2025-08-28 16:28:32 -0500

    updated scripts

--- src/trainEnpactModel.R
+++ src/trainEnpactModel.R
@@ -7,12 +7,13 @@ suppressPackageStartupMessages(library("optparse"))
 
 option_list <- list(
     make_option("--train_data_file", help='data to train with enet'),
-    make_option("--rds_file", help='.rds file to be created as the model'),
-    make_option("--nfolds", type="integer", default=5L, help='How many cv folds?')
+    make_option("--model_rds_file", help='.rds file to be created as the model'),
+    make_option("--nfolds", type="integer", default=5L, help='How many cv folds?'),
+    make_option("--weights_file", help='data to train with enet'),
+    make_option("--metadata", help='data to train with enet')
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
-
 
 library(glue)
 library(R.utils)
@@ -20,69 +21,44 @@ library(data.table)
 library(glmnet)
 library(doParallel)
 library(parallel)
+library(tidyverse)
 
 seed <- 2023
 if(file.exists(opt$train_data_file)){
-    print(glue('INFO - Reading train data...'))
     dt_train <- data.table::fread(opt$train_data_file)
+    print(glue('INFO - Read training data, features = {ncol(dt_train) - 2}'))
 } else {
     stop(glue('ERROR - Training data cannot be found.'))
 }
 
+dt_train <- dplyr::distinct(dt_train)
 # remove missing values
 cc <- complete.cases(dt_train)
 dt_train <- dt_train[cc, ]
 
 # split the data
-X_train <- dt_train[, -c(1,2,3)] |> as.matrix()
-y_train <- dt_train[, c(1,2,3)] |> as.data.frame()
-vbc <- y_train$binding_counts
-#nbc <- (vbc - min(vbc))/(max(vbc) - min(vbc)) # min-max normalization
-nbc <- log10(1 + y_train$binding_counts)
+X_train <- dt_train[, -c(1,2)] |> as.matrix()
+y_train <- dt_train[, c(1,2)] |> as.data.frame()
 
-cl <- 12 #parallel::makeCluster(5)
+cl <- 6 #parallel::makeCluster(5)
 print(glue('INFO - Found {parallel::detectCores()} cores but using {cl}'))
-
-set.seed(seed)
-
 doParallel::registerDoParallel(cl)
+
 print(glue('INFO - training enet model'))
-
-train_methods <- c('logistic')
-
-parallel::mclapply(train_methods, function(each_method){
-
-    cl <- 6 #parallel::makeCluster(5)
-    doParallel::registerDoParallel(cl)
-
-    print(glue('INFO - Starting to build {each_method} enet model'))
-
-    if(each_method == 'linear'){
-
-        cv_model <- tryCatch({
-            glmnet::cv.glmnet(x=X_train, y=nbc, family = "gaussian", type.measure = "mse", alpha = 0.5, keep=T, parallel=T, nfolds=opt$nfolds)
-        }, error = function(e){
-            print(glue('ERROR - {e}'))
-            return(NULL)
-        })
-        save_name <- paste0(opt$rds_file, '.linear.rds', sep='') #gsub('.rds', '.linear.rds', opt$rds_file)
-    } else if (each_method == 'logistic'){
-
-        cv_model <- tryCatch({
-            glmnet::cv.glmnet(x=X_train, y=y_train$binding_class, family = "binomial", type.measure = "auc", alpha = 0.5, keep=T, parallel=T, nfolds=opt$nfolds, trace.it=F)
-        }, error = function(e){
-            print(glue('ERROR - {e}'))
-            return(NULL)
-        })
-        save_name <- opt$rds_file #gsub('.rds', '.logistic.rds', opt$rds_file)
-    }
-    print(cv_model)
-    print(glue('INFO - Saving `{save_name}`'))
-    #rds_file <- glue('{model_file_basename}.{each_method}.rds')
-    saveRDS(cv_model, file=save_name)
-    doParallel::stopImplicitCluster()
-
-}, mc.cores=1)
-
-print(glue('INFO - Finished with model training and saving'))
+set.seed(seed)
+cv_model <- glmnet::cv.glmnet(x=X_train, y=y_train$binding_class, family = "binomial", type.measure = "auc", alpha = 0.5, keep=T, parallel=T, nfolds=opt$nfolds, trace.it=F)
+    
+print(cv_model)
+print(glue('INFO - Saving the model to `{opt$model_rds_file}`'))
+saveRDS(cv_model, file=opt$model_rds_file)
 doParallel::stopImplicitCluster()
+
+weights <- stats::coef(cv_model, s = 'lambda.1se')[-1]
+weights <- weights %>% as.data.frame()
+colnames(weights) <- opt$metadata  
+weights <- weights %>% 
+    dplyr::mutate(feature = paste0('f_', seq_len(nrow(.)))) %>% 
+    dplyr::relocate(feature)
+
+print(glue('INFO - Saving the weights'))
+data.table::fwrite(weights, file=glue("{opt$weights_file}"), sep='\t', row.names=F, col.names=T, quote=F, compress = 'gzip')
